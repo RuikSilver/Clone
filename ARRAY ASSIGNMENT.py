071feefacedf3b3d76f5fc178d1374d2a2199b9f
Ryan Burgin <ruiksilver@gmail.com>
2022-04-22 23:57:50 -0600

    finishing

--- ARRAY ASSIGNMENT.py
+++ ARRAY ASSIGNMENT.py
@@ -37,4 +37,4 @@ my_array_length =
 # problem 17. create a variable called array_string with a value of 'The length of my array is ' (spelling, capitilization, and spaces matter)
 array_string = 'value of cars array length'
 # problem 18. print array_string concatenated with my_array_length to the console
-print(my_array_length
+print(my_array_length)
